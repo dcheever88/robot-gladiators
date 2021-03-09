8bec4622090a591d4d974f2fe98a766f8829f6f4
dcheever <cheeverdanny@gmail.com>
2021-03-08 19:58:06 -0600

    initial game setup

--- assets/js/game.js
+++ assets/js/game.js
@@ -45,8 +45,21 @@ var fight = function() {
     }
     // if player chooses to skip
 } else if (promptFight === "skip" || promptFight === "SKIP") {
-    window.alert(playerName + " has chosen to skip the fight!");
-} else {
+    // confirm player wants to skip
+    var confirmSkip = window.confirm("Are you sure you'd like to quit?");
+
+    // if yes (true), leave fight
+    if (confirmSkip) {
+    window.alert(playerName + " has decided to skip this fight. Goodbye!");
+    // subtract money from playerMoney for skipping
+    playerMoney = playerMoney - 2;
+}
+// if no (false), ask question again by running fight() again
+else {
+    fight();
+    }
+}
+else {
     window.alert("You need to choose a valid option. Try again!");
 }
 
