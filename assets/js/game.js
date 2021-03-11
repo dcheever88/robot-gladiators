1d2b7d2c2a168e2bd9910da4b2f872f0d3c6b2ee
dcheever <cheeverdanny@gmail.com>
2021-03-10 21:10:51 -0600

    finished that module

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,11 +7,14 @@ var enemyNames = ["Roberto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-for(var i = 0; i < enemyNames.length; i++) {
-    console.log(enemyNames[i]);
-    console.log(i);
-    console.log(enemyNames[i] + " is at " + i + " index");
-}
+//for(var i = 0; i < enemyNames.length; i++) {
+    console.log(enemyNames);
+    console.log(enemyNames.length);
+    console.log(enemyNames[0]);
+    console.log(enemyNames[3]);
+    // console.log(i);
+    // console.log(enemyNames[i] + " is at " + i + " index");
+// }
 
 
 var fight = function(enemyName) 
@@ -55,6 +58,11 @@ var fight = function(enemyName)
         if (enemyHealth <= 0) 
         {
             window.alert(enemyName + " has died!");
+
+            //award player money for winning
+            playerMoney = playerMoney + 20;
+
+            // leave while() loop since enemy is dead
             break;
         } 
         else 
@@ -89,7 +97,26 @@ var fight = function(enemyName)
 
 // run fight function to start game
 for(var i = 0; i < enemyNames.length; i++) {
+
+    if(playerHealth > 0)
+    {
+    // let player know what round they are in, remember that arrays start at 0 so it needs to have 1 added to it
+    window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
+
+    // pick new enemy to fight based on the index of the enemyNames array
     var pickedEnemyName = enemyNames[i];
+
+    //reset enemyHealth before starting new fight
     enemyHealth = 50;
+
+    // use debugger to pause script from running and check what's going on at that moment in the code
+    // debugger;
+
+    //pass the pickedEnemyName variable's value into the fight function, where it will assume the value of the enemyName parameter
     fight(pickedEnemyName);
+    }
+    // if player isn't alive, stop the game
+    else {
+        window.alert("You have lost your robot in battle! Game Over!");
+    }
 }
